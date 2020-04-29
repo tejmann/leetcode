9efa60be2fadb175998535611e13dfa5a8445457
tejmann <tp3singh@uwaterloo.ca>
2020-04-28 23:47:29 -0400

    question

---
+++ src/Longest Substring Without Repeating Characters.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var currIndex = 0
+        var startIndex = 0
+        var lastStartIndex = 0
+        var ans = 0
+        val hash = hashMapOf<Char, Int>()
+        for (a in s) {
+            if (hash.containsKey(a)) {
+                startIndex = maxOf(hash[a]!! + 1,lastStartIndex)
+                lastStartIndex = startIndex
+            }
+            if (currIndex + 1 - startIndex > ans) ans = currIndex + 1 - startIndex
+            hash[a] = currIndex
+            currIndex += 1
+        }
+        return ans
+    }
+}
